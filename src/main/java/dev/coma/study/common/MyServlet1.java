fb4a3f4ac4536b954e25981800bd3185027b4155
Coma <comasocean@gmail.com>
2025-09-01 01:29:33 +0900

    Update: gitignore

--- src/main/java/dev/coma/study/common/MyServlet1.java
+++ src/main/java/dev/coma/study/common/MyServlet1.java
@@ -1,28 +1,31 @@
-package common;
+package dev.coma.study.common;
 
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
 
-public class MyServlet2 extends HttpServlet {
+@WebServlet(urlPatterns = "/myServlet1")
+public class MyServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  public MyServlet2() {
-    super();
+
+  public MyServlet1() {
+      super();
   }
 
-  @Override
+	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF-8");
+		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		
 		out.println("<html>");
 		out.println("<head>");
-		out.println("<title>MyServlet</title>");
+		out.println("<title>MyServlet1</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>파이팅 코리아!!!</h1>");
+		out.println("<h1>Fighting Korea!!!</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
+
 }
--- src/main/java/dev/coma/study/common/MyServlet2.java
+++ src/main/java/dev/coma/study/common/MyServlet2.java
@@ -1,31 +1,28 @@
-package common;
+package dev.coma.study.common;
 
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet(urlPatterns = "/myServlet1")
-public class MyServlet1 extends HttpServlet {
+public class MyServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-  public MyServlet1() {
-      super();
+       
+  public MyServlet2() {
+    super();
   }
 
-	@Override
+  @Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
+		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
 		out.println("<html>");
 		out.println("<head>");
-		out.println("<title>MyServlet1</title>");
+		out.println("<title>MyServlet</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>Fighting Korea!!!</h1>");
+		out.println("<h1>파이팅 코리아!!!</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
-
 }
